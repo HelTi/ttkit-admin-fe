12d2858d2f811a98764366c8b4c1041018d29edd
helti <helti@qq.com>
2019-12-02 23:00:21 +0800

    tag操作

--- src/pages/article/tags/index.jsx
+++ src/pages/article/tags/index.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { fetchTags } from '@/services/article';
-import {Tag,Card} from 'antd'
+import { Tag, Card, Icon, Input } from 'antd';
 
+const TagColor = {
+  '1': 'magenta',
+  '2': 'red',
+  '3': 'volcano',
+  '4': 'orange',
+  '5': 'gold',
+  '6': 'lime',
+  '7': 'green',
+  '8': 'cyan',
+  '9': 'blue',
+  '10': 'geekblue',
+};
 class ArticleTag extends React.Component {
   state = {
     tags: [],
+    inputVisible: false,
+    inputValue: '',
   };
+
+  handleClose = removedTag => {
+    const tags = this.state.tags.filter(tag => tag !== removedTag);
+    console.log(tags);
+    this.setState({ tags });
+  };
+
+  showInput = () => {
+    this.setState({ inputVisible: true }, () => this.input.focus());
+  };
+
+  handleInputChange = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleInputConfirm = () => {
+    const { inputValue } = this.state;
+    let { tags } = this.state;
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      tags = [...tags, inputValue];
+    }
+    console.log(tags);
+    this.setState({
+      tags,
+      inputVisible: false,
+      inputValue: '',
+    });
+  };
+
+  saveInputRef = input => (this.input = input);
 
   componentDidMount() {
     fetchTags().then(res => {
@@ -15,14 +59,40 @@ class ArticleTag extends React.Component {
       this.setState({
         tags: data.map(item => item.name),
       });
-      console.log(this.state)
+      console.log(this.state);
     });
   }
   render() {
-
+    const { tags, inputVisible, inputValue } = this.state;
     return (
-      <Card bordered={false} title='文章标签'>
-        {this.state.tags.map((tag,index)=>(<Tag closable={true} key={index}>{tag}</Tag>))}
+      <Card bordered={false} title="文章标签">
+        {tags.map((tag, index) => (
+          <Tag
+            onClose={() => this.handleClose(tag)}
+            color={TagColor[(index + 1) % 10]}
+            closable={true}
+            key={index}
+          >
+            {tag}
+          </Tag>
+        ))}
+        {inputVisible && (
+          <Input
+            ref={this.saveInputRef}
+            type="text"
+            size="small"
+            style={{ width: 78 }}
+            value={inputValue}
+            onChange={this.handleInputChange}
+            onBlur={this.handleInputConfirm}
+            onPressEnter={this.handleInputConfirm}
+          />
+        )}
+        {!inputVisible && (
+          <Tag onClick={this.showInput} style={{ background: '#fff', borderStyle: 'dashed' }}>
+            <Icon type="plus" /> New Tag
+          </Tag>
+        )}
       </Card>
     );
   }
