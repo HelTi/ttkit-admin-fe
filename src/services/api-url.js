7694118fe10ea0b5e42118e32a61c031d8af4359
helti <helti@qq.com>
2020-03-10 22:49:58 +0800

    api切换到https

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,8 +1,8 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 const ApiUrl = {
-    ManApiUrl: isDev ? 'http://localhost:8080' : 'http://api.zhuhuilong.com',
-    StaticUrl: isDev ? 'http://localhost:8082' : 'http://www.zhuhuilong.com',
+    ManApiUrl: isDev ? 'http://localhost:8080' : 'https://api.zhuhuilong.com',
+    StaticUrl: isDev ? 'http://localhost:8082' : 'https://www.zhuhuilong.com',
 }
 
 
