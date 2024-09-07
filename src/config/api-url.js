8d587e57b43e1a3c0f9ae439659c8c0ad343c558
“helti” <helti@qq.com>
2024-09-07 16:16:19 +0800

    feat: 地址变更

--- src/config/api-url.js
+++ src/config/api-url.js
@@ -2,7 +2,7 @@ const isDev = process.env.NODE_ENV === 'development'
 
 const ApiUrl = {
     ManApiUrl: isDev ? 'http://localhost:3003' : 'https://api.zhuhuilong.com',
-    StaticUrl: isDev ? 'http://localhost:3003' : 'https://www.zhuhuilong.com',
+    StaticUrl: isDev ? 'http://localhost:3000' : 'https://www.zhuhuilong.com',
 }
 
 
