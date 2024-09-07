4f9a9a7c0fd95133327bed2be5d19e99f911ab80
“helti” <helti@qq.com>
2024-09-07 21:29:11 +0800

    feat: api url

--- src/config/api-url.js
+++ src/config/api-url.js
@@ -1,8 +1,8 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 const ApiUrl = {
-    ManApiUrl: isDev ? 'http://localhost:3003' : 'https://api.zhuhuilong.com',
-    StaticUrl: isDev ? 'http://localhost:3000' : 'https://www.zhuhuilong.com',
+    ManApiUrl: isDev ? 'http://localhost:3003' : 'https://api.ttkit.cn',
+    StaticUrl: isDev ? 'http://localhost:3000' : 'https://www.ttkit.cn',
 }
 
 
